faeb794717249685fff06bc02820891fc1627389
Henry Lee <lee0906@hotmail.com>
2019-02-15 10:41:04 +1100

    Change the default aes keys

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 const config = {
-    aesKey: process.env.AES_KEY || 'aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa',
-    aesIv: process.env.AES_IV || 'aaaaaaaaaaaaaaaa',
+    aesKey: process.env.AES_KEY || '0123456789abcdef0123456789abcdef0123456789abcdef0123456789abcdef',
+    aesIv: process.env.AES_IV || '0123456789abcdef0123456789abcdef',
     port: process.env.PORT || 3000
 };
 
