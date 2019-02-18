f016195f713047dd3f84aaf206c02f36245ae647
Henry Lee <lee0906@hotmail.com>
2019-02-18 11:04:06 +1100

    Use dotenv

--- config.js
+++ config.js
@@ -1,3 +1,7 @@
+var dotenv = require('dotenv');
+
+dotenv.load();
+
 const config = {
     aesKey: process.env.AES_KEY || '0123456789abcdef0123456789abcdef0123456789abcdef0123456789abcdef',
     aesIv: process.env.AES_IV || '0123456789abcdef0123456789abcdef',
